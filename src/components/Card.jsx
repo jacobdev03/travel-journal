debdfa5dc567b004c14c7b98ac57e6dc404db142
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-02-14 22:57:26 +0100

    card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,26 @@
-function Card() {}
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={`/images/${props.imageUrl}`} className="card--image" alt="" />
+      <div className="card--description">
+        <div className="card--location">
+          <h3 className="card--country">
+            <img src="/images/gps.png" alt="" className="card--gps" />
+            {props.location}
+          </h3>
+          <a href={props.googleMapsUrl} className="card--maps">
+            View on google maps
+          </a>
+        </div>
+
+        <h1 className="card--title">{props.title}</h1>
+        <p className="card--dates">
+          {props.startDate} - {props.endDate}
+        </p>
+        <p className="card--about">{props.description}</p>
+      </div>
+    </div>
+  );
+}
 
 export default Card;
