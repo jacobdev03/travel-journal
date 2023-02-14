dd43f5ea6c5d6cadc72e35ed581f2ef57e66454c
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-02-14 22:01:35 +0100

    navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <img src="/images/earth.png" alt="logo" />
+        <h1>my travel journal</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
