c08f212b3f3feb7d8f671cd04ed653da153aa9c9
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-02-14 22:56:10 +0100

    some data

---
+++ Data.jsx
@@ -0,0 +1,13 @@
+export default [
+  {
+    id: 1,
+    title: "Pamukkale",
+    location: "Turkey",
+    googleMapsUrl: "https://goo.gl/maps/SzHfMzVtuuN72t6V6",
+    startDate: "28 Jul, 2022",
+    endDate: "29 Jul, 2022",
+    description:
+      "Pamukkale, meaning 'cotton castle' in Turkish, is a natural site in Denizli Province in southwestern Turkey. The area is famous for a carbonate mineral left by the flowing of thermal spring water.",
+    imageUrl: "pamukkale.jpeg",
+  },
+];
