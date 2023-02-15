227b72fbe5ac62c5ce6b572e82c3a3ef8cd88cd3
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-02-15 18:14:38 +0100

    add more data

--- Data.jsx
+++ Data.jsx
@@ -10,4 +10,39 @@ export default [
       "Pamukkale, meaning 'cotton castle' in Turkish, is a natural site in Denizli Province in southwestern Turkey. The area is famous for a carbonate mineral left by the flowing of thermal spring water.",
     imageUrl: "pamukkale.jpeg",
   },
+  {
+    id: 2,
+    title: "Malaga",
+    location: "Spain",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Punkt+widokowy+Gibralfaro/@36.7254306,-4.419353,14.71z/data=!4m6!3m5!1s0xd72f7c20dd50ffd:0xfd6d454c5145a650!8m2!3d36.722892!4d-4.4086407!16s%2Fg%2F1jkw6k973?coh=164777&entry=tt",
+    startDate: "8 Aug, 2022",
+    endDate: "3 Sep, 2022",
+    description:
+      "Málaga is a municipality of Spain, capital of the Province of Málaga, in the autonomous community of Andalusia. With a population of 578,460 in 2020, it is sixth most populous city in Spain",
+    imageUrl: "malaga.jpg",
+  },
+  {
+    id: 3,
+    title: "Cos",
+    location: "Greece",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Kos/@36.867457,27.2467832,12.62z/data=!4m7!3m6!1s0x14bdfe67b6c08b35:0x9e9cc4f8469bdcdd!8m2!3d36.8915064!4d27.2877228!15sCgpncmVlY2Uga29zkgEGaXNsYW5k4AEA!16zL20vMDFrdl8w?coh=164777&entry=tt",
+    startDate: "7 Aug, 2019",
+    endDate: "14 Aug, 2019",
+    description:
+      "Cos is a Greek island, part of the Dodecanese island chain in the southeastern Aegean Sea. Kos is the third largest island of the Dodecanese by area, after Rhodes and Karpathos",
+    imageUrl: "kos.jpg",
+  },
+  {
+    id: 4,
+    title: "Split",
+    location: "Croatia",
+    googleMapsUrl: "https://goo.gl/maps/wjxUC9u8etVKroZA6",
+    startDate: "10 Aug, 2016",
+    endDate: "20 Aug, 2016",
+    description:
+      "Split is the second-largest city of Croatia, the largest city in Dalmatia and the largest city on the Croatian coast. It lies on the eastern shore of the Adriatic Sea and is spread over a central peninsula and its surroundings.",
+    imageUrl: "split.jpg",
+  },
 ];
